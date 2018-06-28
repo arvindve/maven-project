12ea47aa2db100ea5ecaa9e356a08b058f8d6f1d
Arvind Verma <arvind.verma@hk.ey.com>
2018-06-28 11:51:55 +0800

    Adding jenkins pipeline as a code

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,8 +14,8 @@ public class Greeter {
 
   /**
    *
-   *  @param someone
-   *  @return
+   * @param someone
+   * @return
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
